46a74c7d4073426a1f6ae799b056a583bbf222eb
mierudayo <webalpa14@gmail.com>
2025-05-23 10:20:44 +0900

    once more

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <h2 style={{ textAlign: 'center', padding: '1rem' }}>
-        日本のCO2排出モック地図（簡素版）
+        {/* 日本のCO2排出モック地図（簡素版） */}
       </h2>
       <JapanMap />
     </div>
